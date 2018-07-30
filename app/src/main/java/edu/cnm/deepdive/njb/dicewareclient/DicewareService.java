b039d1502808508ea8497543876246c9ac690371
Nick Bennett <nick@nickbenn.com>
2018-07-30 11:51:19 -0600

    Basic Diceware client app.

---
+++ app/src/main/java/edu/cnm/deepdive/njb/dicewareclient/DicewareService.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.njb.dicewareclient;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
+public interface DicewareService {
+
+  @GET("diceware")
+  Call<String[]> get(@Header("Authorization") String authorization);
+
+}
---
+++ app/src/main/java/edu/cnm/deepdive/njb/dicewareclient/LoginActivity.java
@@ -0,0 +1,60 @@
+package edu.cnm.deepdive.njb.dicewareclient;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity {
+  private static final int REQUEST_CODE = 1000;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    SignInButton signIn = findViewById(R.id.sign_in);
+    signIn.setOnClickListener((v) -> signIn());
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+    if (account != null) {
+      DicewareApplication.getInstance().setSignInAccount(account);
+      switchToMain();
+    }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == REQUEST_CODE) {
+      try {
+        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+        GoogleSignInAccount account = task.getResult(ApiException.class);
+        DicewareApplication.getInstance().setSignInAccount(account);
+        switchToMain();
+      } catch (ApiException e) {
+        Toast.makeText(this, "Unable to sign in. Please check your credentials and connection.",
+            Toast.LENGTH_LONG).show();
+      }
+    }
+  }
+
+  private void signIn() {
+    Intent intent = DicewareApplication.getInstance().getSignInClient().getSignInIntent();
+    startActivityForResult(intent, REQUEST_CODE);
+  }
+
+  private void switchToMain() {
+    Intent intent = new Intent(this, MainActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+    startActivity(intent);
+  }
+
+}
