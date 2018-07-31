225a32ab3eee0319b77324a91507c15146d94402
nicklocicero <nicklocicero@gmail.com>
2018-07-31 13:15:00 -0600

    It's working. The package name was updated in the gradle file.

---
+++ app/src/main/java/edu/cnm/deepdive/nsl_dicewareclient/DicewareService.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.nsl_dicewareclient;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
+public interface DicewareService {
+
+  @GET("diceware")
+  Call<String[]> get(@Header("Authorization") String authorization);
+
+}
